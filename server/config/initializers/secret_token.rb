f7129c1beb07890c60bc2aded38c7f91814139ba
dustinp <dustinhuypham@gmail.com>
2012-01-09 14:24:02 -0800

    Adding blank rails

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '7afe75c34926fc84f00b1cb94a317d25ea9d310ba860f5722cc9c9084478251057fb24e8ce750568151f5528ae031fd3c2e22e7f3b06a35c86485498cfd5fa60'
